6da863229f716802ec41d76bcf3998d43a043332
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-17 15:50:25 +0100

    Programs

--- list.sh
+++ list.sh
@@ -1,13 +1,12 @@
 # 17-9-2018 JHZ
-
 # Generation of file list and directory
 
 # Sequence data for the Somalogic SOMAscan assay
 R --no-save <<END
 library(readat)
-write.table(aptamers,file="/home/jhz22/work/aptamers.txt",quote=FALSE,row.names=FALSE,sep="\t")
+write.table(aptamers,file="aptamers.txt",quote=FALSE,row.names=FALSE,sep="\t")
 chrpos <- as.data.frame(chromosomalPositions)
-write.table(chrpos,file="/home/jhz22/work/chrpos.txt",quote=FALSE,row.names=FALSE,sep="\t")
+write.table(chrpos,file="chrpos.txt",quote=FALSE,row.names=FALSE,sep="\t")
 END
 
 export box=/scratch/jhz22/box
