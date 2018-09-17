31996ea7ae62a6c69c873449dfc355d6ccae39b5
Jing Hua Zhao <jhz22@cardio-login1.xcat.cluster>
2018-09-17 15:21:06 +0100

    Programs

--- list.sh
+++ list.sh
@@ -9,3 +9,25 @@ write.table(aptamers,file="/home/jhz22/work/aptamers.txt",quote=FALSE,row.names=
 chrpos <- as.data.frame(chromosomalPositions)
 write.table(chrpos,file="/home/jhz22/work/chrpos.txt",quote=FALSE,row.names=FALSE,sep="\t")
 END
+
+export box=/scratch/jhz22/box
+
+# FHS
+
+ls $box/FHS | sed 's/X_//g;s/.txt.gz//g'
+
+# INTERVAL
+
+ls $box/INTERVAL/A1CF.12423.38.3/ | sed 's/12423.38.3_chrom_//g;s/_meta_final_v1.tsv.gz//g'
+
+# KORA
+
+ls $box/KORA | sed 's/KORA_pGWAS.//g;s/.assoc.linear.gz//g'
+
+# Malmo
+
+ls $box/Malmo | sed 's/_summary.csv.gz//g'
+
+# QMDiab
+
+ls $box/QMDiab $box/QMDiab/PGWAS_Results | sed 's/QMDiab_pGWAS.//g;s/assoc.linear.gz//g;s/assoc.linear//g'
