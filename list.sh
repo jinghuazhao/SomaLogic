13b48acc3259508c4ce3e8401ebd8024b1dbb4fc
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-18 09:33:11 +0100

    Programs

--- list.sh
+++ list.sh
@@ -13,22 +13,22 @@ END
 
 export box=/scratch/jhz22/box
 
-# FHS
+# FHS, 1118 items, 16m imputed genotypes
 
 ls $box/FHS | sed 's/X_//g;s/.txt.gz//g'
 
-# INTERVAL
+# INTERVAL, A1CF results for imputed genotypes by chromosome
 
 ls $box/INTERVAL/A1CF.12423.38.3/ | sed 's/12423.38.3_chrom_//g;s/_meta_final_v1.tsv.gz//g'
 
-# KORA
+# KORA, 1131 panel, PLINK outputs by AptamerId
 
 ls $box/KORA | sed 's/KORA_pGWAS.//g;s/.assoc.linear.gz//g'
 
-# Malmo
+# Malmo, 1306 panel, 15m imputed genotypes by EntrezGeneSymbol
 
 ls $box/Malmo | sed 's/^zln//g;s/_summary.csv.gz//g'
 
-# QMDiab
+# QMDiab, PLINK outputs (4+1141) by AptamerId
 
 ls $box/QMDiab $box/QMDiab/PGWAS_Results | sed 's/QMDiab_pGWAS.//g;s/.assoc.linear.gz//g;s/.assoc.linear//g'
