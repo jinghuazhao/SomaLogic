6ea52b4f8e6a2ad91511dafb43068fe40e449fa4
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-18 16:10:31 +0100

    Documents and auxiliary files

---
+++ doc/MDCs.R
@@ -0,0 +1,25 @@
+# Code to immitate Qiong Yang's MDCs_GWASfile_rename.R
+
+pan <-read.csv("MalmoProteomicsKeyCleaned_tab1.csv",as.is=T)
+pan$aptamer <-gsub("-","",pan$SeqId..no.underscore)
+
+mdc <- "/scratch/jhz22/box/Malmo"
+mdc2 <- "/scratch/jhz22/box/FHS"
+mdc.file <-dir(mdc) #1304 files; 1250 are proteins
+mdc.lnk<-data.frame(cbind(mdc.file,gsub("_summary.csv.gz", "",gsub("zln","",mdc.file))))
+names(mdc.lnk)[2]<-"mdcProtein"
+
+pan$mdcProtein<-pan[,"Malmo.Name"]
+
+pan.mdc <-merge(mdc.lnk,pan,by="mdcProtein",all.x=T)
+summary(as.numeric(pan$aptamer))
+
+table(duplicated(pan.mdc$Target)) #no duplicated entry
+sh <- "MDCs.sh"
+unlink(sh)
+for (j in 1:nrow(pan.mdc)){
+		mf <-pan.mdc$mdc.file[j]
+		mf.pid <-paste("X_",pan.mdc$aptamer[j],".summary.csv1.tbl.gz",sep="")
+		cmd <- paste("ln -sf ", mdc,"/",mf," ",mdc2,"/",mf.pid,sep="")
+                cat(cmd,"\n",file=sh,append=TRUE)
+}
