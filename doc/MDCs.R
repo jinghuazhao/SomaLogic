2f19e95ebc6b74aac742d85d39fca6f4954b3716
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-18 21:08:47 +0100

    Documents and auxiliary files

--- doc/MDCs.R
+++ doc/MDCs.R
@@ -1,7 +1,11 @@
 # Code to immitate Qiong Yang's MDCs_GWASfile_rename.R
 
-pan <-read.csv("MalmoProteomicsKeyCleaned_tab1.csv",as.is=T)
-pan <- within(pan, aptamer <-gsub("-","",pan$SeqId..no.underscore))
+pan <-read.csv("MalmoProteomicsKeyCleaned_tab1.csv",as.is=TRUE)
+pan <- within(pan, {
+   aptamer <-gsub("-","",SeqId..no.underscore)
+   mdcProtein <- Malmo.Name
+})
+dim(pan)
 
 mdc <- "/scratch/jhz22/box/Malmo"
 mdc2 <- "/scratch/jhz22/box/FHS"
@@ -9,18 +13,16 @@ mdc.file <-dir(mdc) #1304 files; 1250 are proteins
 mdc.lnk<-data.frame(cbind(mdc.file,gsub("_summary.csv.gz", "",gsub("zln","",mdc.file))))
 names(mdc.lnk)[2]<-"mdcProtein"
 
-pan$mdcProtein<-pan[,"Malmo.Name"]
-
 pan.mdc <-merge(mdc.lnk,pan,by="mdcProtein",all.x=T)
-summary(as.numeric(pan$aptamer))
+summary(as.numeric(with(pan,aptamer)))
+table(duplicated(pan.with(mdc, Target))) #no duplicated entry
 
-table(duplicated(pan.mdc$Target)) #no duplicated entry
 sh <- "MDCs.sh"
 unlink(sh)
 for (j in 1:nrow(pan.mdc))
 {
-   mf <-pan.mdc$mdc.file[j]
-   mf.pid <-paste("X_",pan.mdc$aptamer[j],".summary.csv1.tbl.gz",sep="")
+   mf <-with(pan.mdc,mdc.file)[j]
+   mf.pid <-paste("X_",with(pan.mdc,aptamer)[j],".summary.csv1.tbl.gz",sep="")
    cmd <- paste("ln -sf ", mdc,"/",mf," ",mdc2,"/",mf.pid,sep="")
    cat(cmd,"\n",file=sh,append=TRUE)
 }
