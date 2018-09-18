0143e0b188e2ea929f00b7ef3cac5d5eefd90276
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-18 09:41:38 +0100

    Programs

--- list.sh
+++ list.sh
@@ -1,5 +1,5 @@
-# 17-9-2018 JHZ
-# Generation of file list and directory
+# 18-9-2018 JHZ
+# Generation of file lists and directories
 
 # Sequence data for the Somalogic SOMAscan assay
 R --no-save <<END
@@ -12,23 +12,28 @@ write.table(chrpos[vars],file="chrpos.txt",quote=FALSE,row.names=FALSE,sep="\t")
 END
 
 export box=/scratch/jhz22/box
+export sumstats=/scratch/jhz22/sumstats
+mkdir $sumstats
+cd $sumstats
+mkdir FHS INTERVAL KORA Malmo QMDiab
+cd -
 
 # FHS, 1118 items, 16m imputed genotypes
 
-ls $box/FHS | sed 's/X_//g;s/.txt.gz//g'
+ls $box/FHS | sed 's/X_//g;s/.txt.gz//g' > $sumstats/FHS.list
 
 # INTERVAL, A1CF results for imputed genotypes by chromosome
 
-ls $box/INTERVAL/A1CF.12423.38.3/ | sed 's/12423.38.3_chrom_//g;s/_meta_final_v1.tsv.gz//g'
+ls $box/INTERVAL/A1CF.12423.38.3/ | sed 's/12423.38.3_chrom_//g;s/_meta_final_v1.tsv.gz//g' > $sumstats/INTERVAL.list
 
 # KORA, 1131 panel, PLINK outputs by AptamerId
 
-ls $box/KORA | sed 's/KORA_pGWAS.//g;s/.assoc.linear.gz//g'
+ls $box/KORA | sed 's/KORA_pGWAS.//g;s/.assoc.linear.gz//g' > $sumstats/KORA.list
 
 # Malmo, 1306 panel, 15m imputed genotypes by EntrezGeneSymbol
 
-ls $box/Malmo | sed 's/^zln//g;s/_summary.csv.gz//g'
+ls $box/Malmo | sed 's/^zln//g;s/_summary.csv.gz//g' > $sumstats/Malmo.list
 
 # QMDiab, PLINK outputs (4+1141) by AptamerId
 
-ls $box/QMDiab $box/QMDiab/PGWAS_Results | sed 's/QMDiab_pGWAS.//g;s/.assoc.linear.gz//g;s/.assoc.linear//g'
+ls $box/QMDiab $box/QMDiab/PGWAS_Results | sed 's/QMDiab_pGWAS.//g;s/.assoc.linear.gz//g;s/.assoc.linear//g' > $sumstats/QMDiab.list
