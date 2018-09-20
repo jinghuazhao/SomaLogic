d77e5abca44594b6e57a4743b45ff72beed94e3c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-20 15:30:27 +0100

    Programs

---
+++ metal.sh
@@ -0,0 +1,35 @@
+# 20-9-2018 JHZ
+## generation of individual METAL entries
+
+module load parallel/20131222
+export sumstats=/scratch/jhz22/sumstats
+
+if [ ! -d METAL ]; then mkdir METAL; fi
+
+rm -f $sumstats/METAL/METAL.tmp
+touch $sumstats/METAL/METAL.tmp
+for study in FHS KORA Malmo QMDiab
+do
+   ls $sumstats/$study | sed 's/'"$study".'//g' | awk -vstudy=$study '{
+      s=$1
+      gsub(/.gz|@/,"",s);print s " " ENVIRON["sumstats"] study "/" study "." s ".gz"
+   }' >> METAL/METAL.tmp
+done
+sort -k1,1 METAL/METAL.tmp > METAL/METAL.list
+for p in $(cut -f1 inf1.list)
+do
+   export run=METAL/$p.run
+   echo MARKERLABEL SNPID > $run
+   echo ALLELELABELS EFFECT_ALLELE REFERENCE_ALLELE >> $run
+   echo EFFECTLABEL BETA >> $run
+   echo PVALUELABEL PVAL >> $run
+   echo WEIGHTLABEL N >> $run
+   echo FREQLABEL CODE_ALL_FQ >> $run
+   echo STDERRLABEL SE >> $run
+   echo SCHEME SAMPLESIZE >> $run
+   echo GENOMICCONTROL OFF >> $run
+   echo OUTFILE $HOME/INF/METAL/$p- .tbl >> $run
+   echo $p | join METAL/METAL.list - | awk '{$1="PROCESS"; print}' >> $run;
+   echo ANALYZE >> $run
+   echo CLEAR >> $run
+done
