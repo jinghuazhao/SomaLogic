d6352ee7d6b1f4ad832218f91392447690c27718
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-09-22 19:47:55 +0100

    Generation of auxiliary files from curated databases

--- SomaLogic.R
+++ SomaLogic.R
@@ -5,23 +5,26 @@
 # Comments - it is sensible to work with SeqID to avoid uncertainty in UniProt ID at this stage
 
 setwd("doc")
-## from Jimmy|/scratch/public_databases/SOMALOGIC/LatestVersion
+# from Jimmy|/scratch/public_databases/SOMALOGIC/LatestVersion
 m <- read.delim(paste0("SOMALOGIC_Master_Table_160410_1129info.tsv"),as.is=TRUE)
-## 1129 and 1310 SOMAscan assays
+
+# 1129 and 1310 SOMAscan assays
 assay1129 <- read.delim("1129.tsv",as.is=TRUE) # 72, 677 are duplicates
 assay1310 <- read.delim("1310.tsv",as.is=TRUE)
 setdiff(with(assay1129,SOMAmer.SeqID),with(assay1310,SOMAmer.SeqID))
 SOMAmer.SeqID <-unique(c(with(assay1129,SOMAmer.SeqID),with(assay1310,SOMAmer.SeqID)))
 length(SOMAmer.SeqID)
 cat(sort(SOMAmer.SeqID),sep="\n",file="SomaLogic.list")
-## from Adam|Bioconductor
+
+# from Adam|Bioconductor
 library(readat)
 aptamers <- within(aptamers, {apt=1})
 a <- aptamers[c("SomaId","Units","IsIn1310Panel","IsIn1129Panel","PlasmaDilution","SerumDilution")]
 ma <- merge(m,a,by="SomaId")
 library(dplyr)
 mapt <- left_join(m,aptamers)
-## from Qiong|FHS
+
+# from Qiong on FHS and Malmo
 pan <- read.csv("MalmoProteomicsKeyCleaned_tab1.csv",as.is=T)
 pan <- within(pan, aptamer <-gsub("-","",SeqId..no.underscore))
 dim(pan)
