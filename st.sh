60f44b00e1cb466bf4bdae9e8a36832913278a8b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-10-17 10:14:53 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 27-9-2018 JHZ
+# 17-10-2018 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
@@ -10,7 +10,7 @@ git add analysis-plan.md
 git commit -m "Analysis plan"
 git add SomaLogic.R
 git commit -m "Generation of auxiliary files from curated databases"
-git add list.sh format.sh format.sbatch array.sbatch array.subs array.OK metal.sh
+git add list.sh format.sh format.sbatch array.sbatch array.subs metal.sh
 git commit -m "Programs"
 git add st.sh
 git commit -m "st.sh"
