4d3b31a165f600bc42a9c5f08f92f507bc8b1f4e
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2018-12-01 08:47:23 +0000

    Programs

--- metal.sh
+++ metal.sh
@@ -1,43 +1,36 @@
-# 22-9-2018 JHZ
-## generation of individual METAL entries
-
-function module ()
-{
-  eval `/usr/bin/modulecmd bash $*`
-}
-
-module load parallel/20131222
+# 1-12-2018 JHZ
+## generation of protein-specific METAL entries
 
 export SomaLogic=/scratch/jhz22/SomaLogic
 export sumstats=$SomaLogic/sumstats
 export METAL=$SomaLogic/METAL
 
 if [ ! -d $METAL ]; then mkdir $METAL; fi
-
-rm -f $METAL/METAL.tmp
-touch $METAL/METAL.tmp
+(
 for study in FHS KORA Malmo QMDiab
 do
    ls $sumstats/$study | sed 's/'"$study".'//g' | awk -vstudy=$study '{
       s=$1
       gsub(/.gz|@/,"",s);print s " " ENVIRON["sumstats"] study "/" study "." s ".gz"
-   }' >> $METAL/METAL.tmp
+   }'
 done
+) > $METAL/METAL.tmp
 sort -k1,1 $METAL/METAL.tmp > $METAL/METAL.list
 for p in $(cut -f1 doc/SomaLogic.list)
 do
-   export run=METAL/$p.run
-   echo MARKERLABEL SNPID > $run
-   echo ALLELELABELS EFFECT_ALLELE REFERENCE_ALLELE >> $run
-   echo EFFECTLABEL BETA >> $run
-   echo PVALUELABEL PVAL >> $run
-   echo WEIGHTLABEL N >> $run
-   echo FREQLABEL CODE_ALL_FQ >> $run
-   echo STDERRLABEL SE >> $run
-   echo SCHEME SAMPLESIZE >> $run
-   echo GENOMICCONTROL OFF >> $run
-   echo OUTFILE $METAL/$p- .tbl >> $run
-   echo $p | join $METAL/METAL.list - | awk '{$1="PROCESS"; print}' >> $run;
-   echo ANALYZE >> $run
-   echo CLEAR >> $run
+(
+   echo MARKERLABEL SNPID
+   echo ALLELELABELS EFFECT_ALLELE REFERENCE_ALLELE
+   echo EFFECTLABEL BETA
+   echo PVALUELABEL PVAL
+   echo WEIGHTLABEL N
+   echo FREQLABEL CODE_ALL_FQ
+   echo STDERRLABEL SE
+   echo SCHEME SAMPLESIZE
+   echo GENOMICCONTROL OFF
+   echo OUTFILE $METAL/$p- .tbl
+   echo $p | join $METAL/METAL.list - | awk '{$1="PROCESS"; print}'
+   echo ANALYZE
+   echo CLEAR
+) > METAL/$p.run
 done
