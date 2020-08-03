d23f842cedfe715c87f1cf72cd600c3357dd1eab
jinghuazhao <jinghuazhao@hotmail.com>
2020-08-03 10:05:01 +0100

    Documents and auxiliary files

--- doc/INTERVAL-box.R
+++ doc/INTERVAL-box.R
@@ -1,16 +1,16 @@
-# 10-4-2020 JHZ
+# 3-8-2020 JHZ
 
 somalogic <- read.delim("SOMALOGIC_Master_Table_160410_1129info.tsv",as.is=TRUE)
 gwas <- read.csv("SOMALOGIC_GWAS_protein_info.csv",as.is=TRUE)
 vars <- c("SOMAS_ID_round2", "UniProt",
-          "chromosome_name","start_position","end_position","EntrezGeneSymbol","ensembl_gene_id","external_gene_name")
+          "chromosome_number","start_position","end_position","EntrezGeneSymbol","ensembl_gene_id","external_gene_name")
 chrs <- c(paste(1:22),"X","Y")
 library(reshape)
 gwas <- rename(gwas,c(UniProt="UniProts"))
-somalogic <- rename(subset(somalogic[vars],chromosome_name%in%chrs),
-                  c(chromosome_name="chr",start_position="start",end_position="end",
+somalogic <- rename(subset(somalogic[vars],chromosome_number%in%chrs),
+                  c(chromosome_number="chr",start_position="start",end_position="end",
                     EntrezGeneSymbol="symbol",ensembl_gene_id="ensGene",external_gene_name="extGene"))
-gs <- merge(gwas,somalogic,by.x="SOMAMER_ID",by.y="SOMAS_ID_round2")
+gs <- unique(merge(gwas,somalogic,by.x="SOMAMER_ID",by.y="SOMAS_ID_round2"))
 out <- c("chr","start","end","ensGene","UniProt","symbol","TargetFullName","extGene","Target","UniProts","SOMAMER_ID")
 ord <- with(gs,order(chr,start,end))
 write.table(gs[ord,out],file="INTERVAL-box.tsv",quote=FALSE,row.names=FALSE,sep="\t")
---
+++ doc/st.sh
@@ -0,0 +1,15 @@
+# 3-8-2020
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add doc
+git commit -m "Documents and auxiliary files"
+git add SomaLogic_analysis_plan.md
+git commit -m "Analysis plan"
+git add SomaLogic.R
+git commit -m "Generation of auxiliary files from curated databases"
+git add format.* list.sh metal.* QCGWAS.* clump.* qml.* qqman.R
+git commit -m "Programs"
+git push
